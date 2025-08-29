f39f2dabd811a040fe3fdc38b51ee2f94925d67f
Igo Coelho <igojeferson.coelho@gmail.com>
2025-08-29 19:42:52 +0200

    hamcrest assertions

---
+++ java-junit5/src/test/java/com/github/igocoelho/HamcrestAssertionsDemo.java
@@ -0,0 +1,18 @@
+package com.github.igocoelho;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class HamcrestAssertionsDemo {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void assertWithHamcrestMatcher() {
+        assertThat(calculator.subtract(4, 1), is(equalTo(3)));
+    }
+
+}
